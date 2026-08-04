d45ead88186bf8042db9cb313d6e6c515403a488
wookoonngg <guiyomyee@gmail.com>
2026-08-04 13:50:28 +0900

    Division by zero

--- 0807Seminar/Program.cs
+++ 0807Seminar/Program.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 
-namespace ConductorVerification
+namespace ch9
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            byte statusRegister = 0b0000_0100;
-            ushort adcRawValue = 0x2A3C;
 
-            double referencVoltage = 5.0;
-            double measureVoltage = (adcRawValue / 65535.0) * referencVoltage;
 
-            string logPath = @"C:\ATE_Logs\2026_Q3\Wafer_01.log";
-            Console.WriteLine("===================================================================");
-            Console.WriteLine($"LOG PATH\t: {logPath}");
-            Console.WriteLine($"STATUS REG\t : 0x{statusRegister:X2}");
-            Console.WriteLine($"ADC RAW\t\t : {adcRawValue} LSB");
-            Console.WriteLine($"VOLTAGE\t\t : {measureVoltage:F4} V");
-            Console.WriteLine("===================================================================");
+            float f1 = 10.0f;
+            float f2 = 0.0f;
+            float resultfloat = f1 / f2;
+            Console.WriteLine(resultfloat);
+          
+
+            double d1 = -10.0;
+            double d2 = 0.0f;
+            double resultDouble = d1 / d2;
+            Console.WriteLine(resultDouble);    
+
+
+
+
         }
     }
 }

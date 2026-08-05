b1b8d19dd1964ebde063a47cae21f179c7cf0012
wookoonngg <guiyomyee@gmail.com>
2026-08-05 09:58:00 +0900

    loop statement

--- 0807Seminar/Program.cs
+++ 0807Seminar/Program.cs
@@ -7,27 +7,15 @@ namespace ch9
         static void Main(string[] args)
         {
 
-            int score = 75;
-            string message = (score > 70) ? "합격!" : "불합격!";
-            Console.WriteLine(message);
-
-
-            int menuChoice = 3;
-            switch (menuChoice)
+            for (int i = 1; i <= 10; i++)
             {
-                case 1: 
-                    Console.WriteLine("메뉴 1 선택");
-                    break;
-                case 2:
-                    Console.WriteLine("메뉴 2 선택");
-                    break;
-                case 3:
-                    Console.WriteLine("메뉴 3 선택");
-                    break;
-                default:
-                    Console.WriteLine("잘못된 선택");
-                    break;
-            }
+                if (i == 3) 
+                {
+                    continue; // 아래 콘솔 코드 실행 안함
+                }
+                  Console.WriteLine(i);
+                 }
+
 
 
 

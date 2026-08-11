d28f1ba06b0914f668cda16b0d7fb82014d32f19
박우빈 <woobin2@ati2000.co.kr>
2026-08-11 09:47:50 +0900

    ch21:참조타입ㅍㅣㄹ드

--- 0814Seminar/ch19.cs
+++ 0814Seminar/ch19.cs
@@ -6,23 +6,17 @@ using System.Threading.Tasks;
 
 namespace _0814Seminar
 {
-    class ch19
+    class Vector
     {
+        static void main (string[] args)
+        { 
 
-        private int score; //Score 속성의 배킹필드
-
-        public int Score //Score 속성 
-        {
-            get { return score; } //get함수 score 반환
-
-            set
-            {
-                score = value; //외부에서 할당된 val을 score에 저장
-                if (score < 0) score = 0; //유효한지 검사 음수면 0으로 ㅔㅅ팅
-            }
+        var anonymous = new { Name = "Steve", Age = 34 };
+        Console.WriteLine ($"{anonymous.Name} is {anonymous.Age} years old.");
 
 
         }
+
     }
 
 }
--- 0814Seminar/ch21.cs
+++ 0814Seminar/ch21.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace _0814Seminar
 {
-    internal class ch21
+    struct Wrapper
     {
+        public int[] numbers; //참조 타입 필드
+        public static void Main(string[] args)
+        {
+            Wrapper wrapper = new Wrapper();
+            wrapper.numbers = new int[3] { 1, 2, 3 };
+            UpdateArray(wrapper);
+            Console.WriteLine(wrapper.numbers[1]);
+
+        } 
+
+        public static void UpdateArray(Wrapper wrapper)
+        {
+            wrapper.numbers[1] = 200;
+        }
+
+
+
+
     }
 }

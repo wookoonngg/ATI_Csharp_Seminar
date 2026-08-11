dee01812a383f6badde1e6b4d996b0def25762d9
박우빈 <woobin2@ati2000.co.kr>
2026-08-11 16:32:54 +0900

    ch22:iheritance

--- 0814Seminar/ch21.cs
+++ 0814Seminar/ch21.cs
@@ -14,10 +14,10 @@ namespace _0814Seminar
         }
         public static void Main(string[] args)
         {
-            S[] values = new S[10];
-            S item = values[0];
-            item.Value++;
-            Console.WriteLine(values[0].Value);
+            S[] values = new S[10]; //S  구조체 배열을 기본값으로 일단 생성
+            S item = values[0]; //item에 values[0]복사본이 할당
+            item.Value++; // item 복사본 Value가 증가
+            Console.WriteLine(values[0].Value); // 원본은 변함 없으니 출력 0
 
 
         } 
---
+++ 0814Seminar/ch22.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0814Seminar
+{
+    class Polygon //base class 
+    {
+        public int NumberOfSides { get; set; }
+
+        public Polygon() { NumberOfSides = 0; }
+        public Polygon(int numberOfSides) { NumberOfSides = numberOfSides; }
+
+    }
+
+
+    class Square : Polygon // Polygon 클래스 상속
+    {
+        public float Size { get; set; }
+        public Square(float size)
+        {
+            Size = size;
+            NumberOfSides = 4; // 부모 클래스에서 받은 속성사용 
+
+
+        }
+
+
+        static void Main(string[] arg)
+        {
+
+            Polygon polygon = new Square(4.5f);
+
+            if (polygon is Square)
+            {
+                Square square = (Square)polygon;    
+                Console.WriteLine (square.Size);
+            }
+
+        }
+
+
+
+
+
+
+    }
+}

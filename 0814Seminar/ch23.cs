7bf84f9156bb943812182c9c104a3377b1c8df88
박우빈 <woobin2@ati2000.co.kr>
2026-08-12 10:39:01 +0900

    ch24:interface 1

--- 0814Seminar/ch23.cs
+++ 0814Seminar/ch23.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _0814Seminar
 {
-    internal class ch23
+    public abstract class SearchEngine // 추상 클래스 
     {
+        public abstract string[] Search(string findThis); //추상 메소드 - 본문 없음
+
+
+        public void LogSearchQuery(string query) //추상 클래스 일반 메소드 가지기 가능 
+        {
+            Console.WriteLine($"쿼리 로그: {query}");
+        }
     }
+
+
+    public class BingSearch : SearchEngine // 위에 추상 클래스의 추상 메소드를 구현해야함 
+    {
+        public override string[] Search (string findThis) //override 반드시 필요
+        {
+            LogSearchQuery (findThis); //기반 클래스 일반 메소드 사용가능 
+            Console.WriteLine($"BingSearch가 '{findThis}'를 검색합니다.");
+            return new string[] { "Bing Result X", "Bing Result  Y" };
+        }
+    }
+
+
+
 }
+       
---
+++ 0814Seminar/ch24.cs
@@ -0,0 +1,41 @@
+﻿namespace _0814Seminar
+{
+
+
+    public interface IFileWriter // 인터페이스 -> 멤버 선언만
+    {
+        string Extension { get; } // 구현 없는 속성 (읽기 전용)
+        void Write(string filename_); // 구현 없는 메소드
+    }
+
+
+    public class TextFileWriter : IFileWriter //IFileWriter 인터페이스 구현
+    {
+        public string Extension //override 키워드 사용하지 않음
+        {
+            get { return ".txt"; }
+        }
+
+        public void Write(string filename) // 모든 메소드 public or abstract 이 때 abstract 키워드 사용 불가
+        {
+            Console.WriteLine($"텍스트 파일 '{filename + Extension}'에 내용을 씁니다.");
+        }
+    }
+
+
+    public class RtfFileWriter : IFileWriter
+    {
+        public string Extension
+        {
+            get { return ".rtf"; }
+        }
+
+        public void Write (string filename)
+        {
+            Console.WriteLine($"RTF 파일 '{filename + Extension}'에 내용을 씁니다.");
+        }
+    }
+
+
+
+}
